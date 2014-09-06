524f84bddab66b47441a0e8c383ceb085d228fad
reda test <redatest7@gmail.com>
2014-09-06 22:12:55 +0200

    setting default lang to english -no I18n-

--- mysite/example_bootstrap/settings.py
+++ mysite/example_bootstrap/settings.py
@@ -16,13 +16,13 @@ if 'HEROKU' not in os.environ:
     
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testdb2.sqlite',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+         'ENGINE': 'django.db.backends.sqlite3', 
+         'NAME': 'testdb2.sqlite',               
+         
          'USER': '',
          'PASSWORD': '',
-         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-         'PORT': '',                      # Set to empty string for default.
+         'HOST': '',
+         'PORT': '',
      }
     }   
 
@@ -31,7 +31,7 @@ else:
 
 # DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'fr-fr'
 SITE_ID = 1
 
 MEDIA_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'media'))
@@ -80,11 +80,6 @@ TEMPLATE_DIRS = (
     os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'templates')),
 )
 
-LOCALE_PATHS = ("/home/enigma/django_projects/iqcars/mysite/conf/locale/",)
-
-
-
-
 INSTALLED_APPS = (
     
     'django.contrib.auth',
@@ -93,26 +88,25 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'suit',
+
     'django.contrib.admin',
-    
     'pure_pagination',
     'car_shop',
     'dajaxice',
     'dajax',
-
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
+    # 'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
-    'car_shop.context_processors.strings',
+    # 'car_shop.context_processors.strings',
+    
 )
 
 
@@ -149,71 +143,10 @@ EMAIL_PORT = 587
 
 ACCOUNT_ACTIVATION_DAYS = 14
 
+LANGUAGE_CODE = 'fr'
 
-THUMBNAIL_DEBUG = False
-
-
-
-
-SUIT_CONFIG = {
-    # header
-    'ADMIN_NAME': 'Carshop Administration',
-    # 'HEADER_DATE_FORMAT': 'l, j. F Y',
-    # 'HEADER_TIME_FORMAT': 'H:i',
-
-    # forms
-    'SHOW_REQUIRED_ASTERISK': True,  # Default True
-    # 'CONFIRM_UNSAVED_CHANGES': True, # Default True
-
-    # menu
-    # 'SEARCH_URL': '/admin/auth/user/',
-    # 'MENU_ICONS': {
-    #    'sites': 'icon-leaf',
-    #    'auth': 'icon-lock',
-    # },
-    # 'MENU_OPEN_FIRST_CHILD': True, # Default True
-    # 'MENU_EXCLUDE': ('auth.group','sites',),
-    # 'MENU': (
-    #     'sites',
-    #     {'app': 'auth', 'label':'Authorization', 'icon':'icon-lock', 'models': ('user', 'group')},
-    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
-    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
-    # ),
-
-    # misc
-    # 'LIST_PER_PAGE': 15
-
-    # rename application
-
-
-
-}
-
-
-import logging.config
- 
-MAIN_DIR = os.path.dirname(__file__)
-PROJECT_DIR = MAIN_DIR.rsplit(os.sep, 1)[0]
-
-logging.config.fileConfig( os.path.join(PROJECT_DIR, 'configs', 'logging.conf'))
-
-
-# this part must be add to python files that needs logging
-import logging
-logger = logging.getLogger(__name__)
-logger.info("-------------- begin to crawl ---------------")
-
-
-
-
-USE_I18N = True
-USE_L10N = True
-LANGUAGES = (
-    ('fr', _('French')),
-    ('en', _('English')),
-    ('ar', _('Arabic')),
-)
-DEFAULT_LANGUAGE = 1
+USE_I18N = False
+USE_L10N = False
 
 
 
