def08bf163e347ff764a57efc76a8ebe7d48bd62
reda test <redatest7@gmail.com>
2014-09-06 21:40:17 +0200

    changing the logo

---
+++ mysite/car_shop/jobs.py
@@ -0,0 +1,3 @@
+"Asssistante SAP", "Logisticien/Supply Chain", "Shef de rayon", "Chef de projet informatique", "Asssistante commerciale", "conseiller clientele", "Property manager"
+"chef de secteur", "Approvisionneur", "Cadre en ressource humaine", "Chef de produits", "Controleur de gestion", "Electricien fibre optique", "Formateur", "conducteur de traveaux"
+"Technicien", "magasinier", "Asssistante de direction"
